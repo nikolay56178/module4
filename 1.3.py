584c14c26cf570564a4550bc3a99252334aa73ee
Nikolay56178 <nikolay4156@gmail.com>
2024-02-17 13:59:07 +0300

    'z'

--- 1.3.py
+++ 1.3.py
@@ -1,33 +1,18 @@
-# Смежность вершин
-inc = {
-    1: [2, 8],
-    2: [1, 3, 8],
-    3: [2, 4, 8],
-    4: [3, 7, 9],
-    5: [6, 7],
-    6: [5],
-    7: [4, 5, 8],
-    8: [1, 2, 3, 7],
-    9: [4],
+graph = {
+'0': set(['1','2']),
+'1': set(['0','3','4']),
+'2': set(['0']),
+'3': set(['1']), 
+'4': set(['2','3'])
 }
 
-visited = set()  # Посещена ли вершина?
-Q = []  # Очередь
-BFS = []
+def dfs(graph, start, visited= None):
 
-# Поиск в ширину - ПВШ (Breadth First Search - BFS)
-def bfs(v):
-    if v in visited:  # Если вершина уже посещена, выходим
-        return
-    visited.add(v)  # Посетили вершину v
-    BFS.append(v)  # Запоминаем порядок обхода
-    # print("v = %d" % v)
-    for i in inc[v]:  # Все смежные с v вершины
-        if not i in visited:
-            Q.append(i)
-    while Q:
-        bfs(Q.pop(0))
+    if visited is None:
+        visited = set()
+    visited.add(start)
+    for v in graph[start] - visited:
+        dfs(graph, v, visited)
+    return visited
 
-start = 1
-bfs(start)  # start - начальная вершина обхода
-print(BFS)  # Выводится: [1, 2, 8, 3, 7, 4, 5, 9, 6]
+print(dfs(graph, '1'))
